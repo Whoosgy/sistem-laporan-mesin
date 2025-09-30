fe1e39d43eca3cb2d59d88b5105e0f0ec473bc1d
Whoosgy <ririn.anggraeny.ra@gmail.com>
2025-09-30 12:59:57 +0700

    fixed wm

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -119,13 +119,10 @@
         <main>
             {{ $slot }}
         </main>
-        <div class="py-4">
-            <hr class="my-0 border-gray-200 sm:mx-auto dark:border-gray-700" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-2">
-                Copyright © 2025. Developed by UBSI Team, Ririn & Putri.
-            </span>
-        </div>
-        </footer>
+   <div class="pb-4"> <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+         Copyright © 2025. Developed by UBSI Team, Ririn & Putri.
+     </span>
+</div>
 
 
     </div>
